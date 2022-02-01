20b4d459458777a3fe537d264ac722c7d9fe89af
admin <admin@DESKTOP-JBS34S5>
2022-02-01 13:14:42 +0530

    string buffer

---
+++ stringbuffer/src/stringbuffer/stringbuffer.java
@@ -0,0 +1,31 @@
+package stringbuffer;
+
+import java.util.Scanner;
+
+public class stringbuffer {
+
+	public static void main(String[] args) {
+		Scanner s= new Scanner(System.in);
+		String A=s.next();
+		StringBuffer sb= new StringBuffer(A);
+		for(int i=0;i<A.length();i++)
+		{
+		char ch=A.charAt(i);
+		if(ch>='A' && ch<='Z')
+		{
+		ch=(char)((int)ch+32);
+		sb.setCharAt(i,ch);
+		}
+		else
+		{
+		ch=(char)((int)ch-32);
+		sb.setCharAt(i,ch);
+		}
+		}
+		System.out.println(sb);
+
+
+
+	}
+
+}
